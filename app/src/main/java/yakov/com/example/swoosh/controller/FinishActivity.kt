16103cd88e08b0fb8cb2eecbbab8b3b6720d8414
Yakov Golovin <yakov.golovin@gmail.com>
2020-02-08 23:29:46 +0100

    Lesson 8

--- app/src/main/java/yakov/com/example/swoosh/controller/FinishActivity.kt
+++ app/src/main/java/yakov/com/example/swoosh/controller/FinishActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_finish.*
 import yakov.com.example.swoosh.R
-import yakov.com.example.swoosh.utilities.EXTRA_LEAGUE
-import yakov.com.example.swoosh.utilities.EXTRA_SKILL
+import yakov.com.example.swoosh.model.Player
+import yakov.com.example.swoosh.utilities.EXTRA_PLAYER
 
 class FinishActivity : AppCompatActivity() {
 
@@ -13,11 +13,10 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra(EXTRA_PLAYER) as Player
         searchLeaguesText.text = String.format(
             resources.getString(R.string.search_league_actual),
-            league,
-            skill)
+            player.league,
+            player.skill)
     }
 }
